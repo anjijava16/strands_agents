e89499dcd43136b8b09f73fc37dcc76071f86c9e
Anjaiah Methuku <anjijava16@users.noreply.github.com>
2025-10-20 11:05:29 +0530

    Create 03_agentcore_invoke.sh for agent invocation
    Added script to invoke agent core with a prompt and log responses.

---
+++ agent_core_aws/03_agentcore_invoke.sh
@@ -0,0 +1,16 @@
+(.venv) welcome@jaisairams-Laptop agent_core_aws % agentcore invoke '{"prompt": "Hello"}' 
+✅ MemoryManager initialized for region: us-east-1
+Memory is active, proceeding with invoke
+╭──────────────────────────────────────────────────────────────────────── welcome ─────────────────────────────────────────────────────────────────────────╮
+│ Session: a27ad7ee-b890-4d8a-a85d-45a998fca926                                                                                                            │
+│ Request ID: 2fe72dce-976d-4c29-b958-a9ecad00c80f                                                                                                         │
+│ ARN: arn:aws:bedrock-agentcore:us-east-1:907708980274:runtime/welcome-FKCDEH2f8t                                                                         │
+│ Logs: aws logs tail /aws/bedrock-agentcore/runtimes/welcome-FKCDEH2f8t-DEFAULT --log-stream-name-prefix "2025/10/20/[runtime-logs]" --follow             │
+│       aws logs tail /aws/bedrock-agentcore/runtimes/welcome-FKCDEH2f8t-DEFAULT --log-stream-name-prefix "2025/10/20/[runtime-logs]" --since 1h           │
+│ GenAI Dashboard: https://console.aws.amazon.com/cloudwatch/home?region=us-east-1#gen-ai-observability/agent-core                                         │
+╰──────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────╯
+
+Response:
+Hello! How are you doing today? Is there anything I can help you with?
+
+(.venv) welcome@jaisairams-Laptop agent_core_aws % 
